ef57c5de25948490944063f33e3cd6e2fb3bf09f
Conall <conallimlah@gmail.com>
2021-10-14 21:00:35 +0900

    add scroll nav

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,27 @@ import { Home } from "./views/home";
 import { Projects } from "./views/projects";
 import { Contact } from "./views/contact";
 import "./App.css";
+import { useRef } from "react";
+import { scrollHandler } from "./utils/scrollHandler";
 
 function App() {
+  const homeRef = useRef<HTMLDivElement>(null);
+  const projectsRef = useRef<HTMLDivElement>(null);
+  const contactsRef = useRef<HTMLDivElement>(null);
+
   return (
     <div className="app">
-      <div className="home">
+      <div className="home" ref={homeRef}>
+        <button onClick={() => scrollHandler(projectsRef)}></button>
         <Home />
       </div>
-      <div className="projects">
+      <div className="projects" ref={projectsRef}>
+        <button onClick={() => scrollHandler(homeRef)}></button>
         <Projects />
+        <button onClick={() => scrollHandler(contactsRef)}></button>
       </div>
-      <div className="contact">
+      <div className="contacts" ref={contactsRef}>
+        <button onClick={() => scrollHandler(projectsRef)}></button>
         <Contact />
       </div>
     </div>
