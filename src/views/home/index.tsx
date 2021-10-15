324e788d1e3574bdad2d634ea82ae7d1d6f6fcb4
Conall <conallimlah@gmail.com>
2021-10-15 17:53:01 +0900

    improve home content, make flag responsive

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -2,63 +2,34 @@ import classes from "./home.module.css";
 export const Home = () => {
   return (
     <div className={classes.homeContainer}>
-      <div className={classes.circle}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, qui
-        ipsum pariatur recusandae aliquid cum numquam voluptate unde obcaecati
-        dolor, earum ut est repellat ratione voluptatum sapiente in. Labore,
-        error corporis? Doloribus obcaecati natus dolorum possimus repudiandae
-        qui perferendis hic ullam. Labore reprehenderit dicta doloribus? Ea
-        voluptates maiores quo vitae? Quos delectus dolorem, eius, a laboriosam
-        mollitia dolore rem veniam nobis, dignissimos deleniti? Deleniti veniam
-        perferendis, accusamus, alias, cum sapiente maiores natus possimus
-        voluptate inventore laboriosam excepturi? Adipisci repellat incidunt,
-        cum animi qui libero. Voluptates nostrum voluptatum ut! Fugiat
-        voluptatum ratione ea quos quisquam nemo minima accusantium rem, quas
-        iure dolorum tempore, quasi, veniam mollitia sed molestiae! Iste
-        inventore, ipsa corporis architecto rem minima possimus aut quae nemo
-        {/* ducimus facere recusandae vero perspiciatis molestiae quibusdam aliquid
-        quidem a officia quo! Facilis nihil eligendi, saepe velit aliquam quae
-        dignissimos illo cumque minus possimus rerum? Temporibus reiciendis
-        eveniet nesciunt sed, delectus ipsum id qui quos culpa vitae officiis
-        libero laborum harum fugit neque. Quam sapiente consequatur repudiandae
-        repellat facilis sint eum expedita, excepturi eaque ad maxime labore
-        ipsum velit id molestias incidunt optio eius, dolorem debitis aperiam?
-        Perspiciatis praesentium possimus et expedita dolorem explicabo
-        veritatis tempore molestiae consectetur nostrum animi eius, perferendis
-        aperiam consequatur distinctio quam itaque voluptas quaerat provident
-        molestias. Nam, nulla voluptates, ullam minus quia voluptatibus cum
-        animi mollitia distinctio recusandae enim facilis vitae. Vel magnam amet
-        perferendis impedit explicabo? Sed, sapiente assumenda dolor, dolore
-        unde ratione vitae cum neque perferendis harum officia magni!
-        Praesentium nemo molestiae ipsam? Provident corrupti, non iusto voluptas
-        minus repellat quis praesentium, quo nulla, optio in dolor totam
-        accusamus ab est dolores dolore itaque officia aperiam adipisci minima
-        quod eum esse. Odit dolores necessitatibus consequuntur earum voluptates
-        ea a laboriosam? Impedit cupiditate culpa natus maxime nesciunt
-        blanditiis porro illo neque, exercitationem odit soluta ipsa, ea
-        consectetur. Doloremque impedit recusandae molestias accusantium quam a
-        repudiandae. Voluptas fuga dicta repudiandae vero, maiores harum enim
-        consectetur. Repellendus laboriosam minima incidunt, sit consectetur
-        aspernatur soluta illum nostrum dolorum exercitationem nulla et
-        perferendis dicta eos aliquid pariatur beatae. Labore, iusto rerum
-        aliquam deserunt aut delectus nobis voluptate quod repellat, itaque at
-        quis! Temporibus officiis voluptate pariatur qui doloribus nam, quidem,
-        delectus dolor molestias facere reprehenderit aut quisquam et?
-        Reiciendis, suscipit sint fugiat excepturi iste sit ex voluptate, totam
-        quae, reprehenderit omnis. Amet in molestias fugiat aperiam veritatis
-        vitae accusantium error minus repudiandae, nesciunt, tenetur quas quasi
-        placeat veniam non! Eius tempora voluptas adipisci, illo modi quis quas
-        officia eum aut asperiores incidunt, impedit voluptatem recusandae?
-        Similique doloremque tempore at consequatur, alias cumque voluptatibus
-        voluptas quaerat sed cum vitae modi accusamus aliquid aut assumenda est
-        rem debitis perspiciatis dignissimos necessitatibus. Impedit rerum
-        eveniet laudantium, fugiat dignissimos facere culpa omnis architecto
-        perferendis minus. Aliquid non praesentium impedit magnam laboriosam
-        quisquam dolores. Quisquam enim aliquam natus corrupti qui, est quos ab
-        voluptas minus pariatur cumque officiis laborum commodi facilis tenetur
-        distinctio veritatis, dolore suscipit autem. Suscipit voluptatem quo
-        ipsa eius reiciendis reprehenderit nihil excepturi, debitis molestias,
-        sit quaerat hic consequatur. Laboriosam, commodi veritatis. */}
+      <div className={classes.circleContainer}>
+        <div className={classes.circle}>
+          <div className={classes.circleText}>
+            <span>{`const Conall = me; const Conall = me; const Conall = me;`}</span>
+            <span>{`let atHome = "Tokyo, Japan."; let atHome = "Tokyo, Japan.";`}</span>
+            <span>{`while (building) {Conall.code();} while (building) {Conall.code();}`}</span>
+            <span>{`if (hungry) {Conall.eat();} if (hungry) {Conall.eat();}`}</span>
+            <span>{`else {Conall.sleep();} else {Conall.sleep()} else {Conall.sleep()} `}</span>
+            <span>{`const Conall = me; const Conall = me; const Conall = me;`}</span>
+            <span>{`let atHome = "Tokyo, Japan."; let atHome = "Tokyo, Japan.";`}</span>
+            <span>{`while (building) {Conall.code();} while (building) {Conall.code();}`}</span>
+            <span>{`if (hungry) {Conall.eat();} if (hungry) {Conall.eat();}`}</span>
+            <span>{`else {Conall.sleep();} else {Conall.sleep()} else {Conall.sleep()} `}</span>
+            <span>{`const Conall = me; const Conall = me; const Conall = me;`}</span>
+            <span>{`let atHome = "Tokyo, Japan."; let atHome = "Tokyo, Japan.";`}</span>
+            <span>{`while (building) {Conall.code();} while (building) {Conall.code();}`}</span>
+            <span>{`if (hungry) {Conall.eat();} if (hungry) {Conall.eat();}`}</span>
+            <span>{`else {Conall.sleep();} else {Conall.sleep()} else {Conall.sleep()} `}</span>
+            <span>{`const Conall = me; const Conall = me; const Conall = me;`}</span>
+            <span>{`let atHome = "Tokyo, Japan."; let atHome = "Tokyo, Japan.";`}</span>
+            <span>{`while (building) {Conall.code();} while (building) {Conall.code();}`}</span>
+            <span>{`if (hungry) {Conall.eat();} if (hungry) {Conall.eat();}`}</span>
+            <span>{`else {Conall.sleep();} else {Conall.sleep()} else {Conall.sleep()} `}</span>
+            <span>{`const Conall = me; const Conall = me; const Conall = me;`}</span>
+            <span>{`let atHome = "Tokyo, Japan."; let atHome = "Tokyo, Japan.";`}</span>
+            <span>{`while (building) {Conall.code();} while (building) {Conall.code();}`}</span>
+          </div>
+        </div>
       </div>
     </div>
   );
