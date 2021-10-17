19e031fe793c48ae2c638a932a42d82608a0e1c4
Conall <conallimlah@gmail.com>
2021-10-17 10:33:47 +0900

    add home content and styling

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -8,6 +8,13 @@ import classes from "./home.module.scss";
 export const Home: React.FC = () => {
   return (
     <div className={classes.homeContainer}>
+      <h1 className={classes.title}>Hi, I'm Conall;</h1>
+      <h3>A software engineer based in Tokyo, Japan.</h3>
+      <h1 className={classes.japaneseTitle}>こんにちは、コナルです。</h1>
+      <h3 className={classes.japaneseSubtitle}>
+        東京で活動しているエンジニアです。
+      </h3>
+      {/* TODO: try float left of circle <div><p float> */}
       <div className={classes.circleContainer}>
         <div className={classes.circle}>
           <div className={classes.circleText}>
