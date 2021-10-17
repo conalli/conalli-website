ec85fb585a879190c6b53fe5a58f8b2bf8cced9d
Conall <conallimlah@gmail.com>
2021-10-17 21:46:33 +0900

    update classes, add welcome message

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,19 +1,35 @@
 import classes from "./home.module.scss";
 import { Circle } from "./components/Circle";
 import { useRef } from "react";
+import { motion } from "framer-motion";
 
 export const Home: React.FC = () => {
   const circleRef = useRef<HTMLDivElement>(null);
+  const day = new Date();
+  const dayMap: { [key: number]: string } = {
+    0: "Sunday",
+    1: "Monday",
+    2: "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+    5: "Friday",
+    6: "Saturday",
+  };
   return (
-    <div className={classes.homeContainer}>
+    <motion.div className={classes.homeContainer}>
       <h1 className={classes.title}>Hi, I'm Conall;</h1>
-      <h3>A software engineer based in Tokyo, Japan.</h3>
+      <h3 className={classes.subTitle}>
+        A software engineer based in Tokyo, Japan.
+      </h3>
+      <p className={classes.textBody}>
+        It's good to see you this fine {dayMap[day.getDay()]}.
+      </p>
       <h1 className={classes.japaneseTitle}>こんにちは、コナルです。</h1>
       <h3 className={classes.japaneseSubtitle}>東京で活動しているエンジニア</h3>
       {/* TODO: try float left of circle <div><p float> */}
       <div className={classes.circleContainer} ref={circleRef}>
         <Circle circleRef={circleRef} />
       </div>
-    </div>
+    </motion.div>
   );
 };
