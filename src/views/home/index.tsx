38a0ab81155f1ae3fa102b28ba0cd7bbebf55078
Conall <conallimlah@gmail.com>
2021-10-17 16:03:06 +0900

    refactor components

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,47 +1,20 @@
-import { Linkedin } from "./components/spanComponents/LinkedinSpan";
-import { Location } from "./components/spanComponents/LocationSpan";
-import { Github } from "./components/spanComponents/GithubSpan";
-import { Food } from "./components/spanComponents/FoodSpan";
-import { Sleep } from "./components/spanComponents/SleepSpan";
 import classes from "./home.module.scss";
+import { Circle } from "./components/Circle";
+import { useRef } from "react";
 
 export const Home: React.FC = () => {
+  const circleRef = useRef<HTMLDivElement>(null);
   return (
     <div className={classes.homeContainer}>
       <h1 className={classes.title}>Hi, I'm Conall;</h1>
       <h3>A software engineer based in Tokyo, Japan.</h3>
       <h1 className={classes.japaneseTitle}>こんにちは、コナルです。</h1>
       <h3 className={classes.japaneseSubtitle}>
-        東京で活動しているエンジニアです。
+        東京で活動しているエンジニア。
       </h3>
       {/* TODO: try float left of circle <div><p float> */}
-      <div className={classes.circleContainer}>
-        <div className={classes.circle}>
-          <div className={classes.circleText}>
-            <Linkedin />
-            <Location />
-            <Github />
-            <Food />
-            <Sleep />
-            <Linkedin />
-            <Location />
-            <Github />
-            <Food />
-            <Sleep />
-            <Linkedin />
-            <Location />
-            <Github />
-            <Food />
-            <Sleep />
-            <Linkedin />
-            <Location />
-            <Github />
-            <Food />
-            <Sleep />
-            <Linkedin />
-            <Location />
-          </div>
-        </div>
+      <div className={classes.circleContainer} ref={circleRef}>
+        <Circle circleRef={circleRef} />
       </div>
     </div>
   );
