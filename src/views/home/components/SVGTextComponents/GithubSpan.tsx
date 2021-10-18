a3d68003893ae29db202c0798572bcb353c4fe3c
Conall <conallimlah@gmail.com>
2021-10-18 19:58:52 +0900

    add link to projects

--- src/views/home/components/SVGTextComponents/GithubSpan.tsx
+++ src/views/home/components/SVGTextComponents/GithubSpan.tsx
@@ -1,14 +1,23 @@
+import { Link } from "react-router-dom";
 import classes from "../circle.module.scss";
 export const Github: React.FC = () => {
   return (
     <tspan className={classes.circleText}>
-      while (building){" "}
+      while (
+      <Link to="/projects" className={classes.circleLink}>
+        building
+      </Link>
+      ){" "}
       <a
         className={classes.circleLink}
         href="http://www.github.com/conalli"
         target="_blank"
       >{`{Conall.code();}`}</a>{" "}
-      while (building){" "}
+      while (
+      <Link to="/projects" className={classes.circleLink}>
+        building
+      </Link>
+      ){" "}
       <a
         className={classes.circleLink}
         href="http://www.github.com/conalli"
