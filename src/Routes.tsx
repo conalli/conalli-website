6564d0da3f3093b0f0aefe525e0fd960cad45178
Conall <conallimlah@gmail.com>
2021-10-18 23:44:08 +0900

    use location for AnimatePresence

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,25 +1,24 @@
 import { AnimatePresence } from "framer-motion";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Route, Switch, useLocation } from "react-router-dom";
 import { Contact } from "./views/contact";
 import { Home } from "./views/home";
 import { Projects } from "./views/projects";
 
 export const Routes = () => {
+  const location = useLocation();
   return (
-    <Router>
-      <AnimatePresence exitBeforeEnter>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/projects">
-            <Projects />
-          </Route>
-          <Route exact path="/contact">
-            <Contact />
-          </Route>
-        </Switch>
-      </AnimatePresence>
-    </Router>
+    <AnimatePresence exitBeforeEnter>
+      <Switch location={location} key={location.pathname}>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/projects">
+          <Projects />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+      </Switch>
+    </AnimatePresence>
   );
 };
