ad83b712f37f76aecfb157c784c004cacdd20094
Conall <conallimlah@gmail.com>
2021-10-18 19:59:11 +0900

    add basic animations to nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,24 +1,35 @@
 import { GitHub, LinkedIn } from "@mui/icons-material";
+import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import classes from "./Nav.module.scss";
 
 export const Nav: React.FC = () => {
   return (
     <ul className={classes.navList}>
-      <li>
+      <motion.li whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.9 }}>
         <Link to="/projects">Projects</Link>
-      </li>
-      <li>
+      </motion.li>
+      <motion.li whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.9 }}>
         <Link to="/contact">Contact Me</Link>
-      </li>
+      </motion.li>
       <li>Socials</li>
       <li className={classes.socials}>
-        <a href="http://github.com/conalli" target="_blank">
+        <motion.a
+          href="http://github.com/conalli"
+          target="_blank"
+          whileHover={{ scale: 1.2 }}
+          whileTap={{ scale: 0.9 }}
+        >
           <GitHub fontSize="large" />
-        </a>
-        <a href="http://www.linkedin.com/in/conalli" target="_blank">
+        </motion.a>
+        <motion.a
+          href="http://www.linkedin.com/in/conalli"
+          target="_blank"
+          whileHover={{ scale: 1.2 }}
+          whileTap={{ scale: 0.9 }}
+        >
           <LinkedIn fontSize="large" />
-        </a>
+        </motion.a>
       </li>
     </ul>
   );
