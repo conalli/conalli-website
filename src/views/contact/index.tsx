d6c2c7282cb1c09ce0c366a613c9f5a4c52046b8
Conall <conallimlah@gmail.com>
2021-10-19 20:07:03 +0900

    add basic contact component

--- src/views/contact/index.tsx
+++ src/views/contact/index.tsx
@@ -1,4 +1,6 @@
+import { Nav } from "../../components/Nav";
 import { ViewLayout } from "../../components/ViewLayout";
+import classes from "./contact.module.scss";
 
 export const Contact: React.FC = () => {
   return (
@@ -9,7 +11,18 @@ export const Contact: React.FC = () => {
         color: "black",
       }}
     >
-      <div>Contact</div>
+      <div className={classes.contactContainer}>
+        <div className={classes.aboutMeContainer}>
+          <h1>About me.</h1>
+          <p>
+            I am an engineer originally from the UK, but currently based in
+            Tokyo, Japan. I like JavaScript, TypeScript, React and Go... not
+            necessarily in that order.
+          </p>
+          <Nav />
+        </div>
+        <div className={classes.contactFormContainer}></div>
+      </div>
     </ViewLayout>
   );
 };
