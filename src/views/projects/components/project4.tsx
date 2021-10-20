160770283d0d5cd5e91fa2c452e1f93db47ffc9a
Conall <conallimlah@gmail.com>
2021-10-20 23:47:34 +0900

    create basic layout for projects page

--- src/views/projects/components/project4.tsx
+++
@@ -1,7 +0,0 @@
-export const Project4: React.FC = () => {
-  return (
-    <div>
-      <h1>Project4</h1>
-    </div>
-  );
-};
--- src/views/projects/index.tsx
+++ src/views/projects/index.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
+import { Nav } from "../../components/Nav";
 import { ViewLayout } from "../../components/ViewLayout";
+import { multi } from "../../utils/multiClass";
 import { Project1 } from "./components/project1";
 import { Project2 } from "./components/project2";
 import { Project3 } from "./components/project3";
-import { Project4 } from "./components/project4";
 import classes from "./projects.module.scss";
 
 export const Projects: React.FC = () => {
@@ -32,38 +33,36 @@ export const Projects: React.FC = () => {
     >
       <div className={classes.projectsContainer}>
         {projectPage == 0 && (
-          <div className={classes.projects}>
+          <div className={multi(classes.mainContent, classes.projects)}>
             <h1>Projects</h1>
-            <button onClick={() => clickHandler(1)}>Project1</button>
           </div>
         )}
         {projectPage == 1 && (
-          <div className={classes.project1}>
-            <button onClick={() => clickHandler(0)}>Projects</button>
+          <div className={multi(classes.mainContent, classes.project1)}>
             <Project1 />
-            <button onClick={() => clickHandler(2)}>Project2</button>
           </div>
         )}
         {projectPage == 2 && (
-          <div className={classes.project2}>
-            <button onClick={() => clickHandler(1)}>Project1</button>
+          <div className={multi(classes.mainContent, classes.project2)}>
             <Project2 />
-            <button onClick={() => clickHandler(3)}>Project3</button>
           </div>
         )}
         {projectPage == 3 && (
-          <div className={classes.project3}>
-            <button onClick={() => clickHandler(2)}>Project2</button>
+          <div className={multi(classes.mainContent, classes.project3)}>
             <Project3 />
-            <button onClick={() => clickHandler(4)}>Project4</button>
           </div>
         )}
-        {projectPage == 4 && (
-          <div className={classes.project4}>
-            <button onClick={() => clickHandler(3)}>Project3</button>
-            <Project4 />
-          </div>
-        )}
+        <div className={classes.projectViz}>Project Viz</div>
+        <div className={classes.techStack}>Tech Stack</div>
+        <div className={classes.projectNav}>
+          <button onClick={() => clickHandler(0)}>Projects</button>
+          <button onClick={() => clickHandler(1)}>Project1</button>
+          <button onClick={() => clickHandler(2)}>Project2</button>
+          <button onClick={() => clickHandler(3)}>Project3</button>
+        </div>
+        <div className={classes.pageNav}>
+          <Nav />
+        </div>
       </div>
     </ViewLayout>
   );
